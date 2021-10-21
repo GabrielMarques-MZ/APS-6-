66c58c20f95a9d13bfd17d78ae428f638388443c
EdsonRodrigues1994 <edinho.1994@hotmail.com>
2021-10-20 23:45:07 -0300

    Resolvendo bug do dropdown

--- front-end/js/selectFilter.js
+++ front-end/js/selectFilter.js
@@ -10,51 +10,75 @@ const locaisObject = {
   }
 }
 
-var locaisSel = document.getElementById("locais");
-var enderecoSel = document.getElementById("endereco");
-var produtosSel = document.getElementById("produtos");
-var insumosSel = document.getElementById("insumos");
-var tratoresSel = document.getElementById("tratores");
+let locaisSel = document.getElementById("locais");
+let enderecoSel = document.getElementById("endereco");
+let produtosSel = document.getElementById("produtos");
+let insumosSel = document.getElementById("insumos");
+let tratoresSel = document.getElementById("tratores");
 
 // Carremento dos selects
 window.onload = function() {
 
   
-  for (var x in locaisObject) {
+  for (let x in locaisObject) {
     locaisSel.options[locaisSel.options.length] = new Option(x, x);
   }
   locaisSel.onchange = function() {
     //empty produtoss- and enderecos- dropdowns
     enderecoSel.length = 1;
     //display correct values
-    for (var y in locaisObject[this.value]) {
+    for (let y in locaisObject[this.value]) {
       enderecoSel.options[enderecoSel.options.length] = new Option(y, y);
     }
   }
 }
 
 // Recebimento dos dados do formulario
-var botao = document.getElementById('botao');
-var tabela = document.getElementById('tabela');
+let botao = document.getElementById('botao');
+let tabela = document.getElementById('tabela');
+let idBotao = document.getElementById("botao")
+console.log(idBotao)
 
+// Verifica se existem campos vazios
 botao.addEventListener('click', (e) => {
-  if (produtosSel.value !== '3') {
-    e.preventDefault();
-    console.log('Envie o form');
+  if (locaisSel.value === '1') {
+    alert('Insira um valor no campo Localização');
+  } else if (enderecoSel.value === '2'){
+      alert('Insira um valor no campo Endereço');
+  } else if (produtosSel.value === '3'){
+    alert('Insira um valor no campo Produtos');
+  } else if (insumosSel.value === '4'){
+    alert('Insira um valor no campo Insumos');
+  } else if (tratoresSel.value === '5'){
+    alert('Insira um valor no campo Tratores');
 
-    var locais = locaisSel.value;
-    var endereco = enderecoSel.value;
-    var produtos = produtosSel.value;
-    var insumos = insumosSel.value;
-    var tratores = tratoresSel.value;
-    console.log(locais, endereco, produtos);
+    
+  } else{
+    idBotao.addEventListener("click", abreModal);
+
+    function abreModal(){
+      $("#modal-mensagem").modal('toggle');
+    }
+
+    e.preventDefault();
+
+    let locais = locaisSel.value;
+    let endereco = enderecoSel.value;
+    let produtos = produtosSel.value;
+    let insumos = insumosSel.value;
+    let tratores = tratoresSel.value;
 
     let td = document.createElement("td");
     tabela.innerHTML =  `<td>${locais}</td><td>${endereco}</td><td>${produtos}</td><td>${insumos}</td><td>${tratores}</td>` ;
     tabela.append(td);
-  } else {
-    alert('Preencha todos os campos');
   }
 });
 
+function resetaCampos(){
+  locaisSel.value = "1";
+  enderecoSel.value = "2";
+  produtosSel.value = "3";
+  insumosSel.value = "4";
+  tratoresSel.value = "5";
+}
 
