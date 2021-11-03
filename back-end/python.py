e32cbc9cc80c3e5cf34b6c9d34683a117820e169
EdsonRodrigues1994 <edinho.1994@hotmail.com>
2021-11-03 19:19:43 -0300

    back-end incluso

--- back-end/python.py
+++ back-end/python.py
@@ -1,30 +1,31 @@
+import tkinter as tk
+from tkinter import filedialog
+
 import cv2 as cv
 import os
-import pathlib
-import numpy as np
-import imutils
 
+acesso = False
+root = tk.Tk()
+root.withdraw()
 
-class APIDigital():
+dim = (1000, 1200)
 
-    def procurarDigital(self, userFile):
-        digitais = []
-        dim = (1000, 1200)
+userFile = filedialog.askopenfilename()
 
-        userImg = cv.resize(cv.Canny(cv.imread(userFile), 125, 175), dim)
-        uContours, uHierarchies = cv.findContours(
-            userImg, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
+# -- Inicio do processamento da imagem -- #
+userImg = cv.resize(cv.Canny(cv.imread(userFile), 125, 175), dim)
+uContours, uHierarchies = cv.findContours(userImg, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
 
-        path = str(pathlib.Path(__file__).parent.resolve()="\\database")
+for files in os.listdir('database'):
+    f = os.path.join('database', files)
+    if os.path.isfile(f):
+        baseImg = cv.resize(cv.Canny(cv.imread(f), 125, 175), dim)
+        bContours, bHierarchies = cv.findContours(baseImg, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
+        if len(uContours) == len(bContours):
+            print('Bem vindo!')
+            break
+        else:
+            print('Digital não encontrada no sistema!')
 
-        for files in os.listdir(path):
-            f = os.path.join(path, files)
-            if os.path.isfile(f):
-                for angle in np.arange(0, 360, 15):
-                    baseImg = imutils.rotate(
-                        cv.resize(cv.Canny(cv.imread(f), 125, 175), dim, angle))
-                    bContours, bHierarchies = cv.findContours(
-                        baseImg, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLES)
-                    if len(uContours) == len(bContours):
-                        digitais.append(f.split("\\")[-1])
-        return digitais
+cv.waitKey(0)
+
