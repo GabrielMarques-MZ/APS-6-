6a128a04349bfa0634d50b0399918140166a3fdf
Gabriel <bielmarques922@gmail.com>
2021-11-11 22:44:17 -0300

    Ajuste no popup e no header

--- front-end/js/loginImage.js
+++ front-end/js/loginImage.js
@@ -5,6 +5,14 @@ let images = fetch("json/images.json")
 
 const usuarios = ['user1', 'user2', 'user3']
 
+let botaoLogin = document.querySelector(".btn_login")
+let textLogin1 = document.getElementById("texto_login1")
+let autenticaLogin1 = document.getElementById("autentica_login")
+let pegaCampo1 = document.getElementById("campo_login1");
+let pegaFoto1 = document.getElementById("foto_login1");
+let pegaBtn = document.getElementById("btn_login");
+let pegaBtnText = document.querySelector(".btn_login p");
+
 const fileSelector = document.getElementById('input-file');
 fileSelector.addEventListener('change', (event) => {
 const file = event.target.files[0];
@@ -14,7 +22,13 @@ reader.onloadend = () => {
   const index = images.allImages.findIndex((img) => (img === reader.result))
   const usuario = usuarios[index]
   if (!usuario) {
-    alert('usuario não cadastrado')
+    alert("Usuário ou senha incorretos");
+    textLogin1.style.display = "block";
+    textLogin1.classList.add("pisca");
+    pegaCampo1.style.borderColor = "#de3535";
+    pegaFoto1.style.borderColor = "#de3535";
+    pegaBtn.style.borderColor = "#de3535";
+    pegaBtnText.style.color = "#FFF";
   }else if(usuario === 'user1'){
     location.href = "/front-end/user1.html"
     sessionStorage.setItem("auth", 'user1');
